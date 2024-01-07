0e205020e76dd22c60067c0ef99a4db27c74faf6
DESKTOP-OPTSMH4\ksimp <auekan.k@gmail.com>
2024-01-07 12:13:01 +0700

    fix indent

--- app.js
+++ app.js
@@ -4,24 +4,22 @@ const multer = require('multer');
 const app = express();
 const port = 3000;
 
-const storage = multer.memoryStorage();
 const upload = multer({ dest: 'uploads/' })
 
-  app.post('/upload-file', upload.single('file'), function (req, res, next) {
+app.post('/upload-file', upload.single('file'), function (req, res, next) {
 
-    if (!req.file) return res.status(422).json({ error: "Please select a file" });
-    const payload = req.file;
-    
-    console.log(payload);
-    const result = {
-      fileName: payload.originalname,
-      size: payload.size,
-      extension: payload.mimetype
-    };
-  
-    res.status(200).json(result);
-  })
+  if (!req.file) return res.status(422).json({ error: "Please select a file" });
+  const payload = req.file;
+
+  const result = {
+    fileName: payload.originalname,
+    size: payload.size,
+    extension: payload.mimetype
+  };
+
+  res.status(200).json(result);
+})
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
